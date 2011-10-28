f97d86a82b907654e5833bd065549821072c71b1
Dmitry S. Kravtsov <idkravitz@gmail.com>
2011-10-29 01:26:19 +1100

    YAAAAAAZZZZZZZZZ

--- global_toolkit_server/Main.hs
+++ global_toolkit_server/Main.hs
@@ -1,17 +1,20 @@
 module Main where
 import Prelude hiding (catch)
-import Network (listenOn, accept, sClose, Socket, withSocketsDo, PortID(..))
+import Network (listenOn, accept, sClose, Socket, withSocketsDo, PortID(..), HostName, PortNumber)
 import System.IO
 import System.Environment (getArgs)
-import Control.Exception  (finally, catch)
+import Control.Applicative
+import Control.Exception  (finally, catch, SomeException)
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Monad (forM, filterM, liftM, when)
--- code stolen from
--- http://sequence.complete.org/node/258
 
-type Client = (TChan String, Handle)
+type Client = (TChan String, Handle, ClientInfo)
+type ClientInfo = (HostName, PortNumber)
 
+
+catchAll :: IO a -> (SomeException -> IO a) -> IO a
+catchAll = catch
 
 start servSock = do
     acceptChan <- atomically newTChan
@@ -21,7 +24,7 @@ start servSock = do
 clientLoop :: Handle -> TChan String -> IO ()
 clientLoop handle chan =
     listenLoop (hGetLine handle) chan
-                    --`catch` (const $ return ())
+                    `catchAll` const (return ())
                     `finally` hClose handle
 
 listenLoop :: IO a -> TChan a -> IO ()
@@ -33,8 +36,11 @@ acceptLoop servSock chan = do
     (cHandle, host, port) <- accept servSock
     cChan <- atomically newTChan
     cTID  <- forkIO $ clientLoop cHandle cChan
-    atomically $ writeTChan chan (cChan, cHandle)
+    atomically $ writeTChan chan (cChan, cHandle, (host, port))
     acceptLoop servSock chan
+
+clientInfo :: ClientInfo -> String
+clientInfo (h, p) = h ++ ":" ++ (show p)
 
 mainLoop :: Socket -> TChan Client -> [Client] -> IO ()
 mainLoop servSock acceptChan clients = do
@@ -42,29 +48,29 @@ mainLoop servSock acceptChan clients = do
                       `orElse`
                       (Right `fmap` tselect clients)
     case r of
-        Left (ch,h) -> do
-            putStrLn "new client"
-            mainLoop servSock acceptChan $ (ch,h):clients 
-        Right (line,_) -> do
+        Left client@(ch,h,ci) -> do
+            putStrLn $ "new client [" ++ clientInfo(ci) ++ "]"
+            mainLoop servSock acceptChan $ client:clients
+        Right (line,_,_) -> do
             putStrLn $ "data: " ++ line
             clients' <- forM clients $
-                         \(ch,h) -> do
+                         \client@(ch,h,_) -> do
                             hPutStrLn h line
                             hFlush h
-                            return [(ch,h)]
-                         --`catch` const (hClose h >> return [])
+                            return [client]
+                         `catchAll` const (hClose h >> return [])
             let dropped = length $ filter null clients'
             when (dropped > 0) $
                 putStrLn ("clients lost: " ++ show dropped)
             mainLoop servSock acceptChan $ concat clients'
 
-tselect :: [(TChan a, t)] -> STM (a, t)
-tselect = foldl orElse retry . map (\(ch, ty) -> (flip (,) ty) `fmap` readTChan ch)
+tselect :: [(TChan a, t, c)] -> STM (a, t, c)
+tselect = foldl orElse retry . map (\(ch, ty, ci) -> (,,) <$> (readTChan ch) <*> (return ty) <*> (return ci))
 
 main :: IO ()
 main = withSocketsDo $ do
     [portStr] <- getArgs
-    let port = fromIntegral $ (read portStr :: Int)
-    servSock <- listenOn $ PortNumber port
+    let port = fromIntegral (read portStr :: Int)
+    servSock <- listenOn (PortNumber port)
     putStrLn $ "listening on: " ++ show port
     start servSock `finally` sClose servSock
